0a5aac51142cd03835c75b92693f9762fd7002e3
dmoneyballa <drew.gilliesbc@gmail.com>
2022-10-15 23:23:39 +0000

    I hope I am not as dumb as I suspect I am

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -21,7 +21,6 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	cp finder-test.sh $(TARGET_DIR)/bin/
 	$(INSTALL) -d 0755 $(@D)/ $(TARGET_DIR)/bin/
 	$(INSTALL) -d 0755 $(@D)/finder-app $(TARGET_DIR)/bin/
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
