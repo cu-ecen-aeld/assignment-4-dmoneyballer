b25cb6716b03511c7d6e92002bd1e0b211efa5e2
dmoneyballa <drew.gilliesbc@gmail.com>
2022-10-15 21:37:18 +0000

    worked on my machine

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -16,10 +16,14 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	cp finder-test.sh $(TARGET_DIR)/bin/
+	$(INSTALL) -d 0755 $(@D)/ $(TARGET_DIR)/bin/
+	$(INSTALL) -d 0755 $(@D)/finder-app $(TARGET_DIR)/bin/
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
