9c8de7f4bc46ba2094f80c11c7edb8ab3bc4b111
dmoneyballa <drew.gilliesbc@gmail.com>
2022-10-16 05:31:46 +0000

    try try again

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,7 +10,8 @@ AESD_ASSIGNMENTS_VERSION = 384848212dfddca7529e1aba8aba97024abd84bc
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com: 
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-dmoneyballer.git
+#AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-dmoneyballer.git
+AESD_ASSIGNMENTS_SITE = https://github.com/cu-ecen-aeld/assignments-3-and-later-dmoneyballer.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 AESD_ASSIGNMENTS_INSTALL_TARGET = YES
