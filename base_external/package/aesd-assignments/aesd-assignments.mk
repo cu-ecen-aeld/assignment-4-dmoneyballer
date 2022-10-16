463283f109ca562b007942eced4adb558158daaf
dmoneyballa <drew.gilliesbc@gmail.com>
2022-10-16 05:10:02 +0000

    i am up late this has to wurk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -20,12 +20,11 @@ endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755  $(@D)/finder-app/finder $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755  $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755  $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/ 
-	$(INSTALL) -D -m 0755  $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755  $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin/ 	
-	$(INSTALL) -d 0755 $(@D)/finder-app $(TARGET_DIR)/bin/
+	$(INSTALL) $(TARGET_CONFIGURE_OPTS) -D -m 0755  $(@D)/finder-app/finder $(TARGET_DIR)/usr/bin/
+	$(INSTALL) $(TARGET_CONFIGURE_OPTS) -D -m 0755  $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
+	$(INSTALL) $(TARGET_CONFIGURE_OPTS) -D -m 0755  $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/ 
+	$(INSTALL) $(TARGET_CONFIGURE_OPTS) -D -m 0755  $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin/
+	$(INSTALL) $(TARGET_CONFIGURE_OPTS) -D -m 0755  $(@D)/finder-app/writer.sh $(TARGET_DIR)/usr/bin/ 	
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
